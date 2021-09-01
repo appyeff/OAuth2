8e58003b611799946cf9dd0687bf0999bc9b6787
Yeferson AP <app.yeff@gmail.com>
2021-09-01 18:02:57 -0500

    version inicial

---
+++ src/main/java/com/example/curso/dao/IUserDao.java
@@ -0,0 +1,10 @@
+package com.example.curso.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.curso.entity.User;
+
+public interface IUserDao extends CrudRepository<User, Long>{
+	
+	public User findByUserName (String username);
+}
---
+++ src/main/java/com/example/curso/entity/MovimientoBancario.java
@@ -0,0 +1,48 @@
+package com.example.curso.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="movimiento_bancario")
+public class MovimientoBancario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private Long userID;
+	private String importe;
+	private String name;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserID() {
+		return userID;
+	}
+	public void setUserID(Long userID) {
+		this.userID = userID;
+	}
+	public String getImporte() {
+		return importe;
+	}
+	public void setImporte(String importe) {
+		this.importe = importe;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
